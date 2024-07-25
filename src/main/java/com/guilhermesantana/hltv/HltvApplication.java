877cb2efecc0c4f0ffa6c687c56b38bfc92834fb
Guilherme Santana <guiguilsantana@gmail.com>
2024-07-25 17:45:10 -0300

    feat: initialization project

---
+++ src/main/java/com/guilhermesantana/hltv/HltvApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermesantana.hltv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HltvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HltvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermesantana/hltv/HltvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermesantana.hltv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HltvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
