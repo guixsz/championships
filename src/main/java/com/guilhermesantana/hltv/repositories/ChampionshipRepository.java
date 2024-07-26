bb0b1f9a645bf1ec175d1a4c5fac13898f4ca283
Guilherme Santana <guiguilsantana@gmail.com>
2024-07-26 13:50:04 -0300

    feat: created ChampionshipRepository and query inner join with table address

---
+++ src/main/java/com/guilhermesantana/hltv/repositories/ChampionshipRepository.java
@@ -0,0 +1,16 @@
+package com.guilhermesantana.hltv.repositories;
+
+import com.guilhermesantana.hltv.domain.championship.Championship;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ChampionshipRepository extends JpaRepository<Championship, UUID> {
+
+    @Query("SELECT e FROM Championship e JOIN e.address a")
+    List<Championship> findChampionshipsAndAddress();
+}
